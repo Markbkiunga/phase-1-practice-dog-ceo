5eecd7d9f48346e45f62a0c71c5c307fb4f4a9ad
Mark Brian <markbkiunga2017@gmail.com>
2024-07-12 16:42:35 +0300

    Completes Challenge 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 console.log('%c HI', 'color: firebrick');
+//Challenge 1
 const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'; //Gets 4 random dog images
 let imagesContainer = document.getElementById('dog-image-container');
-fetch('https://dog.ceo/api/breeds/image/random/4')
+fetch(imgUrl)
   .then((response) => response.json())
   .then((data) => {
     imagesArray = data.message;
@@ -10,4 +11,18 @@ fetch('https://dog.ceo/api/breeds/image/random/4')
       imagesContainer.appendChild(imageElement);
       imageElement.setAttribute('src', image);
     });
+  });
+
+//Challenge 2
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+let breedsContainer = document.getElementById('dog-breeds');
+fetch(breedUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    breeds = data.message;
+    for (let breed in breeds) {
+      let breedName = document.createElement('li');
+      breedName.innerText = breed;
+      breedsContainer.appendChild(breedName);
+    }
   });
